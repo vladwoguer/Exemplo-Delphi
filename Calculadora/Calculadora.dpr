cd8be2ade774f11a50e32da18429cf3013aaaba4
Vladwoguer Bezerra <vladwoguerbezerra@gmail.com>
2016-08-26 22:36:45 -0300

    Calculadora

---
+++ Calculadora/Calculadora.dpr
@@ -0,0 +1,14 @@
+program Calculadora;
+
+uses
+  Vcl.Forms,
+  UnitCalculadora in 'UnitCalculadora.pas' {frmCalculadora};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmCalculadora, frmCalculadora);
+  Application.Run;
+end.
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-﻿# Music Rename
-
-Um pequeno app pra administrar arquivos de música.
+﻿# Exemplos Delphi 10 
 
 Vladwoguer Bezerra 2016
